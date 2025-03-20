4509fcfa1ab1daed47d11aad93832250f6442c5c
Denzel Huijbers <huijbers15@outlook.com>
2025-03-20 11:45:28 +0100

    added mock data and images

---
+++ src/data/categories.js
@@ -0,0 +1,31 @@
+const categories = [
+    {
+      id: 1,
+      name: "All",
+      image: "/images/chicken-salad",
+    },
+    {
+      id: 2,
+      name: "Main",
+      image: "/images/chutney-burger.jpg",
+    },
+    {
+      id: 3,
+      name: "Pasta",
+      image: "/images/spaghetti-assassina.jpg",
+    },
+    {
+      id: 4,
+      name: "Drinks",
+      image: "/images/ginger-ale.jpg",
+    },
+    {
+      id: 5,
+      name: "Dessert",
+      image: "/images/dalgona-dame-blanche.jpg",
+    },
+  ]
+  
+  export default categories
+  
+  
---
+++ src/data/menuItems.js
@@ -0,0 +1,55 @@
+const menuItems = [
+    {
+      id: 1,
+      name: "Pasta Puttanesca",
+      price: 15.49,
+      image: "/images/pasta-puttanesca.JPG",
+      category: "Pasta",
+      inCart: true,
+    },
+    {
+      id: 2,
+      name: "Steak & Avocado Bowl",
+      price: 18.15,
+      image: "/images/steak-avocado.jpg",
+      category: "Main",
+      inCart: true,
+    },
+    {
+      id: 3,
+      name: "Chutney Burger",
+      price: 17.99,
+      image: "/images/chutney-burger.jpg",
+      category: "Main",
+      inCart: true,
+    },
+    {
+      id: 4,
+      name: "Spaghetti all'Assassina",
+      price: 15.45,
+      image: "/images/spaghetti-assassina.jpg",
+      category: "Pasta",
+
+      inCart: true,
+    },
+    {
+      id: 5,
+      name: "Grilled Chicken Salad",
+      price: 14.99,
+      image: "/images/chicken-salad.jpg",
+      category: "Main",
+      inCart: false,
+    },
+    {
+      id: 6,
+      name: "Mediterranean Salad",
+      price: 13.75,
+      image: "/images/mediterranean-salad.jpg",
+      category: "Main",
+      inCart: false,
+    },
+  ]
+  
+  export default menuItems
+  
+  
